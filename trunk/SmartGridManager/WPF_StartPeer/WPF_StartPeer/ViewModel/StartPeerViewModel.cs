52c346c09eda34167752d57d4f0b9af8a9ab295d
pepperav@gmail.com <pepperav@gmail.com@15e6bd29-eb21-35dd-6c70-9e12c62f7f72>
2011-05-11 09:23:47 +0000

    adding WPF_StartPeer project

---
+++ trunk/SmartGridManager/WPF_StartPeer/WPF_StartPeer/ViewModel/StartPeerViewModel.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SmartGridManager.Core.Commons;
+using SmartGridManager;
+using WPF_StartPeer.Command;
+using System.Threading;
+using System.IO;
+using System.Windows;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace WPF_StartPeer.ViewModel
+{
+    class StartPeerViewModel : TraceListener
+    {
+        private string nome;
+        private EnergyType energia;
+        private float enpeak;
+        private float price;
+        private bool producer;
+        string output;
+        
+        private readonly StringBuilder builder;
+
+        Building house;
+
+        public DelegateCommand StartPeer { get; set; }
+        public DelegateCommand SetProducer { get; set; }
+        public DelegateCommand Exit { get; set; }
+
+        public StartPeerViewModel()
+        {
+            this.builder = new StringBuilder();
+
+            this.StartPeer = new DelegateCommand((o) => this.Start(), o => this.canStart);
+            this.SetProducer = new DelegateCommand((o) => this.Producer(), o => this.canSetProducer);
+            this.Exit = new DelegateCommand((o) => this.AppExit(), o => this.canExit);
+
+            //TODO: leggere periodicamente il file output.txt
+        }
+
+        public void Start()
+        {
+            house = new Building(Nome, Energia, EnPeak, Price);
+
+            if (producer)
+            {
+                house.setEnergyLevel(90);
+            }
+
+            Trace.WriteLine("Starting Peer {0} ...", Nome);
+
+            //tmpOutput = "Starting Peer ..." + Nome;
+            //File.WriteAllText("output.txt", tmpOutput);
+
+        }
+
+        public void Producer()
+        {
+            producer = true;
+        }
+
+        private bool canStart
+        {
+            get { return true; }
+        }
+
+        private bool canSetProducer
+        {
+            get { return true; }
+        }
+
+        private bool canExit
+        {
+            get { return true; }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set
+            {
+                nome = value;
+                this.OnPropertyChanged(new PropertyChangedEventArgs("Nome"));
+            }
+        }
+
+        public EnergyType Energia
+        {
+            get { return energia; }
+            set
+            {
+                energia = value;
+                this.OnPropertyChanged(new PropertyChangedEventArgs("Energia"));
+            }
+        }
+
+        public float EnPeak
+        {
+            get { return enpeak; }
+            set
+            {
+                enpeak = value;
+                this.OnPropertyChanged(new PropertyChangedEventArgs("EnPeak"));
+            }
+        }
+
+        public float Price
+        {
+            get { return price; }
+            set
+            {
+                price = value;
+                this.OnPropertyChanged(new PropertyChangedEventArgs("Price"));
+            }
+        }
+
+        public string LeggiFile
+        {
+            get
+            {
+                return output = File.ReadAllText("output.txt");
+            }
+        }
+
+        public void AppExit()
+        {
+            Application.Current.Shutdown();
+        }
+
+        public string MyTrace
+        {
+            get { return this.builder.ToString(); }
+        }
+
+        public override void Write(string message)
+        {
+            this.builder.Append(message);
+            this.OnPropertyChanged(new PropertyChangedEventArgs("MyTrace"));
+        }
+
+        public override void WriteLine(string message)
+        {
+            this.builder.AppendLine(message);
+            this.OnPropertyChanged(new PropertyChangedEventArgs("MyTrace"));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+    }
+}
